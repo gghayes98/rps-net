3e9848226093ded1cec958d96fa3db5a61b15eba
Gabriel <66342077+gghayes98@users.noreply.github.com>
2024-06-07 17:26:04 -0600

    Update network.py
    Hid ip from network.py

--- network.py
+++ network.py
@@ -3,7 +3,7 @@ import pickle
 class Network():
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "10.34.139.28" # Change this to your ipv4 address
+        self.server = # Change this to your ipv4 address
         self.port = 5555
         self.addr = (self.server, self.port)
         self.p = self.connect()
